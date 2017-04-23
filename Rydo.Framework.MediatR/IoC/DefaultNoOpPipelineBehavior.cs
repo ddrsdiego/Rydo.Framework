892a6276995f01253e6d8ed42fc4da0fee787387
ddrsdiego <ddrsdiego@hotmail.com>
2017-04-23 11:32:31 -0300

    Adiciona DefaultNoOpPipelineBehavior

---
+++ Rydo.Framework.MediatR/IoC/DefaultNoOpPipelineBehavior.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System.Threading.Tasks;
+
+namespace Rydo.Framework.MediatR.IoC
+{
+    public class DefaultNoOpPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next)
+        {
+            return next();
+        }
+    }
+}
